bd43ebc6e19a4978f8665848c97106188a9e4641
daverave1212 <crazyman_dave2000@yahoo.com>
2018-05-26 06:35:38 -0700

    Uploaded new project folders

---
+++ XClient/src/github/xclient/XClient.java
@@ -0,0 +1,18 @@
+package github.xclient;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class XClient {
+	
+	public static void main(String[] args) throws Exception{
+		
+		
+		
+		
+		
+	}
+	
+}
